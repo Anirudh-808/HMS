771aad2ccc8f249d7b20901256edd29d13430dfb
Anirudh-808 <aninagaraj808@gmail.com>
2025-05-03 12:13:55 +0530

    second

--- login.c
+++ login.c
@@ -16,10 +16,11 @@ int is_equal(char *str1 , char *str2)
     }
 }
 
-void login()
+void patient_login()
 {
     char users_list[10][50] = {"anirudh"};
     char passwords_list[10][50] = {"ani123"};
+
     char username[50];
     char password[50];
 
@@ -32,7 +33,7 @@ void login()
 
     //checking validity
     int flag = 0;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < sizeof(users_list)/sizeof(char); i++)
     {
         if (is_equal(users_list[i] , username) == 1)
         {
@@ -63,3 +64,52 @@ void login()
     }
 }
 
+void doctor_login()
+{
+    //user data
+    char user_id[10];
+    char password[50];
+
+    //database
+    char users_id_list[][10] = {'123'};
+    char passwords_list[10][50] = {"ani123"};
+
+    //user input
+    printf("ID: ");
+    scanf("%s" , &user_id);
+    printf("PASSWORD: ");
+    scanf("%s" , password);
+    printf("%s" , divider1);
+
+    //checking validity
+    int flag = 0;
+    for (int i = 0; i < sizeof(users_id_list)/sizeof(char); i++)
+    {
+        if (is_equal(users_id_list[i] , user_id) == 1)
+        {
+            if (is_equal(passwords_list[i] , password) == 1)
+            {   
+                flag = 1;
+                printf("LOGIN SUCCESSFUL!\n");
+            }
+            else
+            {   
+                flag = 2;
+                break;
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("INVALID USERID\n");
+    }
+    else if (flag == 2)
+    {
+        printf("INVLAID PASSWORD\n");
+    }
+}
